2c0c47accdce4121d479e2e7b111cdc7009d2a20
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-01-29 18:24:24 +0100

    Añadida beta para comunicar boton con script python

---
+++ botonmusic.ino
@@ -0,0 +1,26 @@
+
+
+int pinBoton = 2;  
+int pinLed =  13;   
+int estadoBoton = 0;  
+ 
+void setup() {
+Serial.begin(9600);
+// Inicializa el pin del LED como salida:
+pinMode(pinLed, OUTPUT);
+// Inicializa el pin del botón como entrada:
+pinMode(pinBoton, INPUT);
+}
+ 
+void loop(){
+  estadoBoton = digitalRead(pinBoton);
+   
+  if (estadoBoton == HIGH) {
+    //if(estadoBoton == LOW){
+       Serial.println("1");
+      digitalWrite(pinLed, HIGH);
+    //}
+  } else {
+     digitalWrite(pinLed, LOW);
+  }
+}
