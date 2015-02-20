7bbb5344ebab32f38dca2f115cbb538feeca22ac
Jonathan Gines <jonathangines@hotmail.com>
2015-02-20 19:21:26 +0100

    version final 20/2

--- touchpad.py
+++ touchpad.py
@@ -4,21 +4,27 @@ import serial
 import time
 import pyglet
 
+flanco = True
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout = 3.0)  #el ttyACM0 puede depender, se mira en el Arduino abajo a la derecha
-song1 = pyglet.media.load('verse2.aif')
-player = pyglet.media.Player();
-player.queue(song1);
+#song1 = pyglet.media.load('verse2.aif')
+song1 = pyglet.resource.media('verse1.mp3')
+song2 = pyglet.resource.media('verse2.mp3')
+song3 = pyglet.resource.media('verse3.mp3')
+
+#player = pyglet.media.Player();
+#player.queue(song1);
 
 def reproducir(id):
+	global flanco
 	if (id == "2"):
-		player.play()
-	elif(id == '3'):
-		player.play()
+		song1.play()
+	elif(id == '3' ):
+		song2.play()
 	elif(id == '4'):
-		player.play()
-
-
-
+		song3.play()
+		
+	#pyglet.app.run()
+	
 while True:
    lineaLeida = arduino.readline()												  
    if lineaLeida:
@@ -28,8 +34,3 @@ while True:
    			reproducir('3')
    		elif lineaLeida == '4\r\n':
    			reproducir('4')
-   		else:
-   			print lineaLeida,
-
-   		
-
