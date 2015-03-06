edebc418d76b6b7fb86dc077e0c475ae08b8a59d
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-03-06 19:35:45 +0100

    La gui funciona sin el arduino

--- gui.py
+++ gui.py
@@ -1,20 +1,47 @@
-import gtk, sys, serial
+import gtk, sys, serial, pygame
+
+pygame.mixer.init()
+sound1 = pygame.mixer.Sound('base.ogg')
+sound2 = pygame.mixer.Sound('verse2.ogg')
+sound3 = pygame.mixer.Sound('verse3.ogg')
+
 
 class PyApp(gtk.Window):
+    
+    def sonido(self, lineaLeida):
+	if (lineaLeida == '2\r\n'):
+	    try:
+		sound1.play(loops = -1);
+		print "ESCRIBO DESPUES DE LA BASE"
+	    except pygame.error, message:
+		print "base" + sound1.name
+		
+
+	elif lineaLeida == '3\r\n':
+	    try:
+		sound2.play(loops = 0);
+	    except pygame.error, message:
+		print "sound2" + sound2.name
+		
+	elif lineaLeida == '4\r\n':
+	    try:
+		sound3.play(loops = 0);
+	    except pygame.error, message:
+		print "sound3" + sound3.name
+	else:
+	    print "Error de lectura (Leo cosas raras)"
    
     def __init__(self):
         super(PyApp, self).__init__()
 
-        arduino = serial.Serial('/dev/ttyACM0', 9600, timeout = 3.0)
-
         def b1(b):
-         arduino.write('3\r\n')
+         self.sonido('3\r\n')
 
-        def b1(b):
-         arduino.write('4\r\n')
+        def b2(b):
+         self.sonido('4\r\n')
 
         def base(b):
-         arduino.write('2\r\n')
+         self.sonido('2\r\n')
         
         self.set_title("Buttons")
         self.set_size_request(400, 300)
