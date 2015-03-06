2ca132718fae138a43cdafd9a8e8597cea0b2585
David Moreno Lumbreras <dmorenolumb@gmail.com>
2015-03-06 18:29:15 +0100

    Ahora usamos otro metodo de pygame que repodrude varias canciones una encima de otra (formato ogg) + prueba de threads

--- touchpad.py
+++ touchpad.py
@@ -3,33 +3,50 @@
 import serial
 import time
 import pygame
+#import thread
+
 pygame.mixer.init()
 flanco = True
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout = 3.0)  #el ttyACM0 puede depender, se mira en el Arduino abajo a la derecha
+sound1 = pygame.mixer.Sound('base.ogg')
+sound2 = pygame.mixer.Sound('verse2.ogg')
+sound3 = pygame.mixer.Sound('verse3.ogg')
 
-#player = pyglet.media.Player();
-#player.queue(song1);
 
 
+def sonido(lineaLeida):
+    try:
+	if (lineaLeida == '2\r\n'):
+	    try:
+		sound1.play(loops = -1);
+		print "ESCRIBO DESPUES DE LA BASE"
+	    except pygame.error, message:
+		print "base" + sound1.name
 		
-	#pyglet.app.run()
-	
+
+	elif lineaLeida == '3\r\n':
+	    try:
+		sound2.play(loops = 0);
+	    except pygame.error, message:
+		print "sound2" + sound2.name
+		
+	elif lineaLeida == '4\r\n':
+	    try:
+		sound3.play(loops = 0);
+	    except pygame.error, message:
+		print "sound3" + sound3.name
+	else:
+	    print "ESTOY LEYENDO BASURA"
+    except OSError:
+	print "Se me ha fastidiao el OS"
+    except serial.serialutil.SerialException:
+	print "Se me ha fastidiao el SERIAL"
+    	
 while True:
    lineaLeida = arduino.readline()												  
    if lineaLeida:
-   		if (lineaLeida == '2\r\n'):
-			pygame.mixer.music.load("verse1.mp3")
-			pygame.mixer.music.play()
-			while pygame.mixer.music.get_busy() == True:
-			    continue
-
-   		elif lineaLeida == '3\r\n':
-			pygame.mixer.music.load("verse2.mp3")
-			pygame.mixer.music.play()
-			while pygame.mixer.music.get_busy() == True:
-			    continue
-   		elif lineaLeida == '4\r\n':
-   			pygame.mixer.music.load("verse3.mp3")
-			pygame.mixer.music.play()
-			while pygame.mixer.music.get_busy() == True:
-			    continue
+	try:
+	    sonido(lineaLeida)
+	    #thread.start_new_thread(sonido, (lineaLeida, ))
+	except:
+	    print "error: fallo al lanzar el thread"
